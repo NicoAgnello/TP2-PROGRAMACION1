e97726498f8f51ed1b9a728fd2c007cdaac687f2
Nicolas <nikoagnello1@gmail.com>
2024-10-10 03:50:48 -0300

    validacion

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 const agregarProductos = () => {
-    let nombreArticulo = document.getElementById('input-articulo').value
-    let cantidad = Number(document.getElementById('input-cantidad').value)
-    let precioUnitario = Number(document.getElementById('input-precio').value)
-    let alicuota = document.getElementById('alicuota').value
-    console.log(typeof(nombreArticulo));
-    console.log(typeof(cantidad));
-    console.log(typeof(precioUnitario));
-    console.log(typeof(alicuota));
+    let nombreArticulo = document.getElementById('input-articulo').value;
+    let cantidad = Number(document.getElementById('input-cantidad').value);
+    let precioUnitario = Number(document.getElementById('input-precio').value);
+    let alicuota = document.getElementById('alicuota').value;
+    let completado = true;
 
-    if (cantidad <= 0 || isNan(cantidad)) {
-        generarTextoDebajo('input-cantidad','Error en la carga de cantidad' )
+    /// Validacion de campos
+    completado = validarCampo('input-articulo', 'artículo', !nombreArticulo || nombreArticulo.length <= 1, completado);
+    completado = validarCampo('input-cantidad', 'cantidad', cantidad <= 0 || isNaN(cantidad) || !cantidad, completado);
+    completado = validarCampo('input-precio', 'precio unitario', precioUnitario <= 0 || isNaN(precioUnitario) || !precioUnitario, completado);
+    completado = validarCampo('alicuota', 'alicuota', !alicuota, completado);
+
+    if (!completado) {
+        document.getElementById('myModal').style.display = 'block';
     }
+};
 
-
-}
-
+const validarCampo = (id, mensajeError, condicion, completado) => {
+    if (condicion) {
+        generarTextoDebajo(id, mensajeError);
+        return false;
+    } else {
+        document.getElementById(`${id}-error`).style.display = 'none';
+        return completado;
+    }
+};
 
 const generarTextoDebajo = (idElement, errorMessage) => {
     let errorElement = document.getElementById(idElement + '-error');
-
     if (!errorElement) {
         errorElement = document.createElement('div');
         errorElement.id = idElement + '-error';
         errorElement.style.color = 'red';
-        errorElement.style.textAlign = 'center'
-        errorElement.style.paddingTop = '5px'
+        errorElement.style.textAlign = 'center';
+        errorElement.style.paddingTop = '5px';
         document.getElementById(idElement).insertAdjacentElement('afterend', errorElement);
     }
-
-    errorElement.textContent = errorMessage;
+    errorElement.textContent = 'Error en la carga de ' + errorMessage;
 };
+
+// Función para cerrar el modal
+const closeModal = () => {
+    document.getElementById('myModal').style.display = 'none';
+};
+// Event listeners para cerrar el modal
+document.getElementById('closeModal').addEventListener('click', closeModal);
+document.getElementById('closeModalSecondary').addEventListener('click', closeModal);
+
+
 
 
 // const limpiarCarro = () => {
-    
+//     // Implementa la lógica para limpiar el carrito
 // }
